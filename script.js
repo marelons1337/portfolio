6aa0fca05c7d86cc20b6722e388dc8cb07058b33
Marelons1337 <jarorafo@gmail.com>
2020-12-08 21:32:17 +0100

    fixed performance issue for background

--- script.js
+++ script.js
@@ -1,7 +1,56 @@
 window.onload = function() {
-    Particles.init({
-      selector: '.background',
-      color: '#4464AD',
-      connectParticles: true
-    });
+  Particles.init
+  ({
+    
+  // normal options
+    selector: 
+  '.background'
+  ,
+    maxParticles: 
+  450
+  ,
+  color:
+  '#4464AD'
+  ,
+    
+  // options for breakpoints
+    responsive: [
+      {
+        breakpoint: 
+  768
+  ,
+        options: {
+          maxParticles: 
+  200
+  ,
+          color: 
+  '#4464AD'
+  ,
+          connectParticles: 
+  false
+        }
+      }, {
+        breakpoint: 
+  425
+  ,
+        options: {
+          maxParticles: 
+  100
+  ,
+        }
+      }, {
+        breakpoint: 
+  320
+  ,
+        options: {
+          maxParticles: 
+  50
+   
+  // smallest screen
+        }
+      }
+    ]
+  });
   };
+
+  
